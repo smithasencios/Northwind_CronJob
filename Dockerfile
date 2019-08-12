9ebfbac8d06f0d27e92274734492767bb352d7fb
Shumaha Smith Asencios Pezo <smithasencios@gmail.com>
2019-08-11 21:50:59 -0500

    add python class

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM debian:stretch-slim
 
-ENV USERNAME=northwind
+# ENV USERNAME=northwind
 
-RUN useradd ${USERNAME} --create-home --shell /bin/bash
+# RUN useradd ${USERNAME} --create-home --shell /bin/bash
 
 RUN apt-get update && \
     apt-get -y install --no-install-recommends \
@@ -29,6 +29,7 @@ RUN sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
     update-locale LANG=en_US.UTF-8
 
 ADD util /util
+ADD provision /provision
 ADD run /run
 
 ENV LANG en_US.UTF-8
@@ -36,4 +37,6 @@ ENV LANGUAGE en_US:en
 ENV LC_ALL en_US.UTF-8
 ENV LANG C.UTF-8
 
-USER ${USERNAME}
+RUN provision/provision.sh
+
+# USER ${USERNAME}
--- util/monitor.py
+++ util/monitor.py
@@ -1,1 +1,38 @@
-print("Estamos ejecutando codigo Python dentro del contenedor")
+import os
+import sys
+import logging
+import pymysql
+
+try:
+
+    # Open database connection
+    db = pymysql.connect(os.environ['DATABASE_HOST'],os.environ['NORTHWIND_DB_USER'],os.environ['NORTHWIND_DB_PASSWORD'],os.environ['NORTHWIND_DB_DATABASE'] )
+
+    # prepare a cursor object using cursor() method
+    cursor = db.cursor()
+
+    # Prepare SQL query to INSERT a record into the database.
+    sql = "SELECT id,order_date,status_id,shipped_date FROM orders \
+            WHERE status_id = 2 AND shipped_date <= date_sub(curdate(),interval 2 day)" 
+    # Execute the SQL command
+    cursor.execute(sql)
+    # Fetch all the rows in a list of lists.
+    results = cursor.fetchall()
+
+    for row in results:
+        order_id = row[0]
+        order_date = row[1]
+        status_id = row[2]
+        shipped_date = row[3]
+        # Now print fetched result
+        print ("order_id = %s,order_date = %s,status_id = %d,shipped_date = %s" % \
+            (order_id, order_date,status_id,shipped_date))
+
+    # disconnect from server
+    db.close()
+
+except Exception as e:
+    logging.error(e.message)
+    sys.exit(4)
+
+sys.exit(0)
